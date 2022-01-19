8439f264ec079ad6d8b47e9b8dd462e6a452554d
Daniel Goldacker <daniel-goldacker@hotmail.com>
2022-01-19 13:21:58 -0300

    Commit inicial do exemplo simples da utilização do RabbitMQ com C#

---
+++ Publicador/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Publicador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
