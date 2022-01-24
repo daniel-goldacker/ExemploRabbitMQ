016591a02cc8da841761eb28253dca5acddf66f4
Daniel Goldacker <daniel-goldacker@hotmail.com>
2022-01-24 14:03:32 -0300

    Adicionado exemplo simples usando RabbitMQ

--- Consumidor/Program.cs
+++ Consumidor/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace Consumidor
 {
@@ -6,7 +9,45 @@ namespace Consumidor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory()
+            {
+                HostName = "Localhost"
+            };
+
+            // Abrimos uma conexão com um nó RabbitMQ
+            using (var connection = factory.CreateConnection())
+            {
+                // Criamos um canal onde vamos definir uma fila, uma mensagem e publicar a mensagem
+                using (var channel = connection.CreateModel())
+                {
+                    // Declaramos a fila apartir da qual vamos consumir as mensagens 
+                 channel.QueueDeclare(queue: "saudacao_1",
+                                      durable: false,
+                                      exclusive: false,
+                                      autoDelete: false,
+                                      arguments: null);
+
+                    // Solicita a entrega das mensagens de forma assíncrona e fornece um retorno de chamada 
+                    var consumer = new EventingBasicConsumer(channel);
+
+    
+                    // Recebe a mensagem da fila converte para string e imprime no console a mensagem 
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("[X] Recebida: {0}", message);
+                    };
+
+                    // Indicamos o consumo da mensagem
+                    channel.BasicConsume(queue: "saudacao_1",
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.ReadLine();
+
+                }
+            }
         }
     }
 }
--- Publicador/Program.cs
+++ Publicador/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
 
 namespace Publicador
 {
@@ -6,7 +8,38 @@ namespace Publicador
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory()
+            {
+                HostName = "Localhost"
+            };
+
+            // Abrimos uma conexão com um nó RabbitMQ
+            using (var connection = factory.CreateConnection())
+            {
+                // Criamos um canal onde vamos definir uma fila, uma mensagem e publicar a mensagem
+                using (var channel = connection.CreateModel())
+                {
+                    // Declaramos a fila apartir da qual vamos consumir as mensagens 
+                    channel.QueueDeclare(queue: "saudacao_1",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    // Criamos a mensagem a ser posta na fila e codificamos a mensagem como um arrayu de bytes 
+                    string message = "Bem vindo ao RabbitMQ";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    // Publicamos a mensagem informando a fila e o corpo da mensagem
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "saudacao_1",
+                                         basicProperties: null,
+                                         body: body);
+
+                    Console.WriteLine("[X] Enviada: {0}", message);
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
